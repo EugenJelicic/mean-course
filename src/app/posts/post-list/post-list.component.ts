e1d5fb21203da420c6a4ee85fd2cf8a833553f70
EugenJelicic <eugen@agilathon.com>
2021-03-18 12:23:38 +0100

    Tutorial module 2 done

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Post } from '../post.model';
+import { Subscription } from 'rxjs';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss'],
 })
-export class PostListComponent implements OnInit {
-  constructor() {}
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postsSub: Subscription = new Subscription;
 
-  ngOnInit(): void {}
+  constructor(public postsService: PostsService) {}
 
-  @Input() posts: Post[] = [];
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService
+      .getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
 
-  //posts = [
-  //  { title: 'First Post', content: "This is the first post's content" },
-  //  { title: 'Second Post', content: "This is the second post's content" },
-  //  { title: 'Third Post', content: "This is the third post's content" },
-  //];
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
 }
