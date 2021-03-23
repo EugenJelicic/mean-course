ed36e916976a62f49b7369b0b93b65669350cefd
EugenJelicic <eugen@agilathon.com>
2021-03-23 16:04:05 +0100

    added edit functionality and refactored everything

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post.create.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { HttpClientModule } from '@angular/common/http';
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -31,6 +31,19 @@ export class PostsService {
       });
   }
 
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
+    this.http
+      .put('http://localhost:3000/api/posts/' + id, post)
+      .subscribe((response) => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex((p) => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
   deletePost(postId: string) {
     this.http
       .delete('http://localhost:3000/api/posts/' + postId)
@@ -45,6 +58,10 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return this.http.get<{_id: string, title: string, content: string}>('http://localhost:3000/api/posts' + id);
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { id: 'null', title: title, content: content };
     this.http
@@ -52,7 +69,7 @@ export class PostsService {
         'http://localhost:3000/api/posts',
         post
       )
-      .subscribe(responseData => {
+      .subscribe((responseData) => {
         const id = responseData.postId;
         post.id = id;
         this.posts.push(post);
